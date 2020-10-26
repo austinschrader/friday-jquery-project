9245177bd132726b9c6835bb28174976a582e21c
Austin Schrader <austin.jschrader@gmail.com>
2020-10-26 12:02:10 -0700

    Add new descriptions, connect scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-  
+	$('form#programming').submit(function (event) {
+		const experienceResult = $('#experience').val();
+		const typeWorkResult = $('#typeWork').val();
+		const hopeToDoResult = $('#hopeToDoResult').val();
+		const experienceLogicResult = $('#experienceLogic').val();
+		const mathAbilityResult = $('#mathAbility').val();
+
+    if experienceResult ==  {
+
+    }
+	});
 });
